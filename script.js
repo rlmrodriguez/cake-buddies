df12fd84b26d51744a418673aec8ef3cf9952f3c
RLMR <roden.mrodriguez@gmail.com>
2021-05-20 15:49:27 +0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,37 @@
+$('document').ready(function () {
+  /* Navbar Menu Toggle */
+  $('.menu-toggle').on('click', () => {
+    $('.menu-toggle').toggleClass('active');
+  });
+
+  /* Owl Carousel */
+  $('.products-crsl').owlCarousel({
+    loop: true,
+    margin: 10,
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: true,
+      },
+      600: {
+        items: 3,
+        nav: false,
+      },
+      1000: {
+        items: 3,
+        nav: true,
+        loop: false,
+      },
+    },
+  });
+
+  //Scroll to top button
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      $('.back-to-top').fadeIn();
+    } else {
+      $('.back-to-top').fadeOut();
+    }
+  });
+});
